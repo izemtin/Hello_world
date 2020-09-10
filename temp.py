c4beb124d614453537ad546b1343386e5121fd53
izemtin <70115903+izemtin@users.noreply.github.com>
2020-09-10 13:29:51 +0300

    hi

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def wtf_is_going_on(a):
+    total = 0 
+    print (total + a)
+
+print (wtf_is_going_on(10))
